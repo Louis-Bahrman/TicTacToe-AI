145d49a1134385c7b3d8ff9ef75b40387d8517c6
Louis Bahrman <louis.bahrman@etu.imt-lille-douai.fr>
2020-04-21 13:57:49 +0200

    Ajout de la selection du MCTS, modification et renommage du type Arbre en ArbreMCTS

--- src/tictactoecodingame/Selection.java
+++ src/tictactoecodingame/Selection.java
@@ -5,15 +5,45 @@
  */
 package tictactoecodingame;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author louis
  */
 public class Selection {
-    ArrayList<indice> indices;
+    ArrayList<Integer> chemin;
     
-            
-    public Selection(double c){
-        
+    public Selection(){
+        chemin=new ArrayList<>();
+    }
+    
+    
+    public Selection(double c, ArbreMCTS a){
+        Selection s=new Selection();
+        chemin=SelectionAux(c, a, s, 0).chemin;
+    }
+    
+    private Selection SelectionAux(double c, ArbreMCTS a, Selection s, int nbSimulPere){
+        if (a.estFeuille()){
+            return s;
+        }
+        else {
+            double coefMax=0;
+            int indiceSelection=0;
+            ArrayList<ArbreMCTS> f=a.getfils();
+            for(int i=0;i<f.size();i++){
+                ArbreMCTS arbreTeste = f.get(i);
+                double coef =arbreTeste.getvalue()+c*Math.sqrt(Math.log(nbSimulPere)/arbreTeste.getFraction().getDenominateur());
+                if (coef>coefMax){
+                    indiceSelection=i;
+                    coefMax=coef;
+                }
+            }
+            ArbreMCTS arbreSelectionne=f.get(indiceSelection);
+            int nbSimulPereNew=arbreSelectionne.getFraction().getDenominateur();
+            s.chemin.add(indiceSelection);
+            return SelectionAux(c,arbreSelectionne,s,nbSimulPereNew);
+        }
     }
 }
